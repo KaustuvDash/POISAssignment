8ff78a44e461f3e78dc873ce6b80d9865e9514c1
KaustuvDash <gtavicecity581@gmail.com>
2022-02-19 08:56:32 +0530

    Assignment1

---
+++ assignment1.py
@@ -0,0 +1,34 @@
+seedSize=7
+moduls = 321
+generator = 123
+def functionL(seedSize):
+    return seedSize**2 - 2*seedSize +1
+def functionH(firstHalf,secondHalf):
+    #first convert firstHalfBinary to  number
+    n = int(firstHalf,2)
+    #to cal g^x % mod and convert it to binaryString
+    p = bin(pow(generator,n) % moduls)
+    p = p.replace('0b','')
+    #now to fill the 0 from front
+    fillSize = seedSize - len(p)
+    for i in range(fillSize):
+        p = '0'+p
+    hard_core_bit = 0
+    for i in range(len(firstHalf)):
+        hard_core_bit = (hard_core_bit ^ (int(firstHalf[i]) & int(secondHalf[i]))) % 2
+    return p + secondHalf + str(hard_core_bit)
+def functionG(seed):
+    result=''
+    binaryString = seed
+    for i in range(functionL(seedSize)):#looping functionL times
+        index = int(len(binaryString)/2)
+        firstHalf = binaryString[:index]
+        secondHalf = binaryString[index:]
+        binaryString = functionH(firstHalf,secondHalf) 
+        result += binaryString[-1]#last bit
+        binaryString = binaryString[:-1]#ignoring last bit
+    return result
+seed = input()
+print(functionG(seed))
+
+
